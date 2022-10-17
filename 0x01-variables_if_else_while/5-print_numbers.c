b1f6579ad632234866e926022343ce151100ca9a
eyramFel <fianyekufelix@live.com>
2022-10-17 14:21:34 -0700

    variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main-programm entry point.
+ * Return:0 no errorm non zero if error
+ */
+int main (void)
+
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	        putchar('\n');
+		return (0);
+}
