9172bf7a66ca2970a42f878a953b6188c0af00ff
eyramFel <fianyekufelix@live.com>
2022-10-17 22:12:52 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>	
+#include <stdio.h>
 /**
- * main - program entry
- * Return: By defaults returns zero if no errors otherwise returns non-zero
+ * main -print a line of code using puts
  *
+ * Return: 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
